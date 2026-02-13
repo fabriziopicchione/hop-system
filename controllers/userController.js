1d499f8deddc31c928f51458035564c760e12a5d
fabriziopicchione <fabrizio.picchione@hilton.com>
2026-02-13 12:03:22 +0100

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -9,6 +9,37 @@ exports.getAll = async (req, res) => {
     }
 };
 
+// Create new user
+exports.create = async (req, res) => {
+    try {
+        const newUser = new User(req.body);
+        await newUser.save();
+        res.status(201).json(newUser);
+    } catch (err) {
+        res.status(400).json({ error: err.message });
+    }
+};
+
+// Update existing user
+exports.update = async (req, res) => {
+    try {
+        const updatedUser = await User.findByIdAndUpdate(req.params.id, req.body, { new: true });
+        res.json(updatedUser);
+    } catch (err) {
+        res.status(400).json({ error: err.message });
+    }
+};
+
+// Delete user
+exports.delete = async (req, res) => {
+    try {
+        await User.findByIdAndDelete(req.params.id);
+        res.json({ message: "User deleted" });
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+};
+
 // Simple auth: verify PIN
 exports.login = async (req, res) => {
     const { pin } = req.body;
